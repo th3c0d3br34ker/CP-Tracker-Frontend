916f47fc1eda5ea187fc083bb8d41333e2790aa9
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-08-17 23:39:37 +0530

    🔧 Fixes (#4)
    * 🔧 Fixes
    
    * 🔧 Fixes

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,27 +1,21 @@
-import React, { Component } from "react";
+import React from "react";
 
 import { Container, Image } from "react-bootstrap";
 
-class Header extends Component {
-  render() {
-    return (
-      <Container fluid={true} className="header-div bg-dark">
-        <div className="row header">
-          <Image
-            className="logo"
-            src={require("../../assets/logo.png")}
-            alt="logo"
-            width="40"
-          />
+export default function Header() {
+  return (
+    <Container fluid={true} className="row header-div">
+      <Image
+        className="logo"
+        src={require("../../assets/logo.png")}
+        alt="logo"
+        width="40"
+      />
 
-          <h2>CP-Tracker</h2>
-        </div>
-      </Container>
-    );
-  }
+      <h2>{"CP-Tracker"}</h2>
+    </Container>
+  );
 }
-
-export default Header;
 
 // TODO:
 // - Modify to API Call.
