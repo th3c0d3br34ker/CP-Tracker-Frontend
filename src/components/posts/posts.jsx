e7f4998729d15385b7ecf556bbe3f7944e14596e
Jainam Desai <jainamvipul.desai2019@vitstudent.ac.in>
2020-08-19 17:32:11 +0530

    ✨ Added Loading Feature (#5)

--- src/components/posts/posts.jsx
+++ src/components/posts/posts.jsx
@@ -1,34 +1,45 @@
 import React, { Component } from "react";
-import axios from "axios";
+import Axios from "axios";
 
 import { Container } from "react-bootstrap";
 
 import PostCard from "./postCard.jsx";
+import Loading from "../../elements/loading/loading.jsx";
 
 class Posts extends Component {
   state = {
     posts: [],
+    isLoading: true,
   };
 
   componentDidMount() {
-    axios
-      .get("https://cp-track.herokuapp.com/api/postcard/")
-      .then((response) => {
-        this.setState({ posts: response.data });
-      })
-      .catch((error) => {
-        console.log(error);
-      });
+    window.scroll({
+      top: 0,
+    });
+    this.setState({ isLoading: true }, () => {
+      console.log("Loading...");
+      Axios.get("https://cp-track.herokuapp.com/api/postcard/")
+        .then((response) => {
+          this.setState({ posts: response.data, isLoading: false });
+        })
+        .catch((error) => {
+          console.log(error);
+        });
+    });
   }
 
   render() {
     return (
       <Container id="practise">
-        <div className="row practice-container-div">
-          {this.state.posts.map((post) => (
-            <PostCard key={post.id} cardInfo={post} />
-          ))}
-        </div>
+        {this.state.isLoading ? (
+          <Loading />
+        ) : (
+          <div className="row practice-container-div">
+            {this.state.posts.map((post) => (
+              <PostCard key={post.id} cardInfo={post} />
+            ))}
+          </div>
+        )}
       </Container>
     );
   }
