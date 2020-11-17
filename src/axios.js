951059777c2335ab2cfeb6e3b79548cb8326fa97
th3c0d3br34ker <jainamvipul.desai2019@vitstudent.ac.in>
2020-11-17 22:27:04 +0530

    Update link

--- src/axios.js
+++ src/axios.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const instance = axios.create({
   baseURL:
-    "https://cors-anywhere.herokuapp.com/https://cp-tracker.herokuapp.com/api",
+    "https://cp-tracker.herokuapp.com/api",
 });
 
 export default instance;
